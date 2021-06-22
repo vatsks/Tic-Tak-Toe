b2f79457e831df7a404af87e652f68de611aa656
vatsks <iamksvats@gmail.com>
2021-06-22 21:39:53 +0530

    Added a freeze response everytime someone has won. Also imageview can be tapped only once.

--- app/src/main/java/com/example/tictaktoe/MainActivity.java
+++ app/src/main/java/com/example/tictaktoe/MainActivity.java
@@ -15,42 +15,47 @@ public class MainActivity extends AppCompatActivity {
     int[][] winningPosition ={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
     //0 circle 1cross 2 empty
+    boolean gameActive =true;
 
 public void dropin(View view) {
 
     ImageView counter;
     counter = (ImageView) view;
-    counter.setTranslationY(-1000);
+
 
     int taggedCounter = Integer.parseInt(counter.getTag().toString());
-    gameState[taggedCounter]=activePlayer;
-    if (activePlayer == 0) {
 
-       activePlayer=1;
-        counter.setImageResource(R.drawable.circle);
-    } else {
-        activePlayer = 0;
+    if (gameState[taggedCounter]==2 && gameActive) {
+        counter.setTranslationY(-1000);
+        gameState[taggedCounter] = activePlayer;
+        if (activePlayer == 0) {
 
-        counter.setImageResource(R.drawable.cross);
-    }
+            activePlayer = 1;
+            counter.setImageResource(R.drawable.circle);
+        } else {
+            activePlayer = 0;
 
-    counter.animate().translationYBy(1000).setDuration(400);
-    for(int[] winningPositio : winningPosition){
-        if (gameState[winningPositio[0]]==gameState[winningPositio[1]]&&gameState[winningPositio[1]]==gameState[winningPositio[2]]&&gameState[winningPositio[0]]!=2){
-              String message;
-            if(activePlayer==0){
-                message="cross";
-            }
-            else
-            {
-                message="circle";
-            }
-
-            Toast.makeText(this,message+" has won",Toast.LENGTH_LONG).show();
+            counter.setImageResource(R.drawable.cross);
         }
+
+        counter.animate().translationYBy(1000).setDuration(400);
+        for (int[] winningPositio : winningPosition) {
+            if (gameState[winningPositio[0]] == gameState[winningPositio[1]] && gameState[winningPositio[1]] == gameState[winningPositio[2]] && gameState[winningPositio[0]] != 2) {
+
+                //someone has won
+                gameActive=false;
+                String message;
+                if (activePlayer == 0) {
+                    message = "cross";
+                } else {
+                    message = "circle";
+                }
+
+                Toast.makeText(this, message + " has won", Toast.LENGTH_LONG).show();
+            }
+        }
+
     }
-
-
 }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
